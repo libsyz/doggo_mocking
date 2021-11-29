e97414fb278589e67c1feb3ccb61d1eabba66abe
Miguel Jimenez <denkatalabeno@gmail.com>
2021-11-29 17:43:10 +0800

    basic testing implemented

--- spec/services/doggo_service_spec.rb
+++ spec/services/doggo_service_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 require 'rspec'
+require 'rspec/mocks'
 
 
 describe DoggoService do
-  it 'gets an image from the doggo api' do
+  xit 'gets an image from the doggo api' do
     res  = DoggoService.call
+    # problem! We are ACTUALLY CALLING AN API
     expect(res).to match(/https:\/\/images\.dog\.ceo\/breeds\/.+\/.+\.jpg/)
   end
+
+  xit 'gets an image from the doggo api - Mocked' do
+    fake_service = double('FakeDoggoService')
+    allow(fake_service).to receive(:call).and_return("https://images.dog.ceo/breeds/german_shepherd/0001.jpg/")
+    expect(fake_service.call).to match(/https:\/\/images\.dog\.ceo\/breeds\/.+\/.+\.jpg/)
+    # this might be useful in an integration scenario
+    # but now it's not testing anything at all
+  end
+
+
 end
